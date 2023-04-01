6a87750d98d8e8e268c30ce50fadbcd697432308
aleburbridge <aleburbridge@gmail.com>
2023-03-31 21:22:55 -0700

    css god

--- jte-classes/gg/jte/generated/ondemand/JteteleprompterGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/JteteleprompterGenerated.java
@@ -5,11 +5,11 @@ import java.util.LinkedHashMap;
 import java.util.List;
 public final class JteteleprompterGenerated {
 	public static final String JTE_NAME = "teleprompter.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,2,3,4,4,4,12,12,12,13,13,13,13,13,13,13,13,13,13,14,14,28};
+	public static final int[] JTE_LINE_INFO = {0,0,1,2,3,4,4,4,12,12,12,13,13,13,13,13,13,13,13,13,14,14,28};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, List<ScriptLine> script, LinkedHashMap<TIPURole, String> roleMap) {
-		jteOutput.writeContent("\r\n<head>\r\n    <title>Telimpromptu</title>\r\n    <link rel=\"stylesheet\" href=\"style.css\">\r\n</head>\r\n<p style=\"font-size: 8em\">\r\n");
+		jteOutput.writeContent("\n<head>\n    <title>Telimpromptu</title>\n    <link rel=\"stylesheet\" href=\"/style.css\">\n</head>\n<div style=\"font-size: 8em\">\n");
 		for (ScriptLine scriptLine: script) {
-			jteOutput.writeContent("\r\n    <span class=\"");
+			jteOutput.writeContent("\n    <span class=\"");
 			jteOutput.setContext("span", "class");
 			jteOutput.writeUserContent(scriptLine.getSpeaker().toLowercaseString());
 				jteOutput.setContext("span", null);
@@ -17,11 +17,10 @@ public final class JteteleprompterGenerated {
 			jteOutput.setContext("span", null);
 			jteOutput.writeUserContent(roleMap.get(scriptLine.getSpeaker()));
 			jteOutput.writeContent(":</span> ");
-			jteOutput.setContext("p", null);
-			jteOutput.writeUserContent(scriptLine.getText().replace("\n", "<br>"));
-			jteOutput.writeContent("<br>\r\n");
+			jteOutput.writeUnsafeContent(scriptLine.getText().replace("\n", "<br>"));
+			jteOutput.writeContent("<br><br>\n");
 		}
-		jteOutput.writeContent("\r\n</p>\r\n\r\n<script>\r\n    const delay = ms => new Promise(res => setTimeout(res, ms));\r\n    const scrollFunction = async () => {\r\n        while (true) {\r\n            await delay(50);\r\n            window.scrollBy(0, 1)\r\n        }\r\n    }\r\n\r\n    scrollFunction()\r\n</script>\r\n</body>");
+		jteOutput.writeContent("\n</div>\n\n<script>\n    const delay = ms => new Promise(res => setTimeout(res, ms));\n    const scrollFunction = async () => {\n        while (true) {\n            await delay(50);\n            window.scrollBy(0, 4)\n        }\n    }\n\n    scrollFunction()\n</script>\n</body>");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		List<ScriptLine> script = (List<ScriptLine>)params.get("script");
