4af92aa56a5da9f63e8a6b802819b8467d26f31b
Chris-Perkins <chrisfromtemporaryid@gmail.com>
2023-05-29 15:07:06 -0700

    fix redundant scripts getting generated (i think)

--- src/main/kotlin/teleimpromptu/script/building/ScriptBuilderService.kt
+++ src/main/kotlin/teleimpromptu/script/building/ScriptBuilderService.kt
@@ -8,12 +8,11 @@ import teleimpromptu.script.parsing.SegmentTag
 object ScriptBuilderService {
     fun buildScriptForPlayerCount(playerCount: Int): MutableList<ScriptSection> {
         val script = mutableListOf<ScriptSection>()
-        script.add(getAvailableSectionsForRoles(playerCount, SegmentTag.INTRODUCTION, script).random())
-        // script.add(getAvailableSectionsForRoles(playerCount, SegmentTag.MAIN_STORY, script).random())
+        script.add(getAvailableSections(playerCount, SegmentTag.INTRODUCTION, script).random())
 
         do {
             // protect against infinite looping here
-            script.add(getAvailableSectionsForRoles(playerCount, SegmentTag.SEGMENT, script).random())
+            script.add(getAvailableSections(playerCount, SegmentTag.SEGMENT, script).random())
         } while (getPrimaryRolesInScript(script).size < playerCount)
 
         if (getPrimaryRolesInScript(script).size > playerCount) {
@@ -24,33 +23,31 @@ object ScriptBuilderService {
             error("There are roles in the script without sections where they are the primary role!?")
         }
 
-        script.add(getAvailableSectionsForRoles(playerCount, SegmentTag.CLOSING, script, true).random())
+        script.add(getAvailableSections(playerCount, SegmentTag.CLOSING, script, true).random())
 
         return script
     }
 
-    // todo its possible to have a section that only mentions a role and that role ends up not having any speaking parts
-    private fun getAvailableSectionsForRoles(playerCount: Int,
-                                             filterByTag: SegmentTag,
-                                             scriptSoFar: List<ScriptSection>,
-                                             canAddNothing: Boolean = false): List<ScriptSection> {
-        val primaryRolesInScript = getPrimaryRolesInScript(scriptSoFar)
+    private fun getAvailableSections(
+        playerCount: Int,
+        filterByTag: SegmentTag,
+        usedSections: List<ScriptSection>,
+        canAddNothing: Boolean = false
+    ): List<ScriptSection> {
         return ScriptParsingService.sections
-            // if it is the type we are looking for
             .filter { it.tags.contains(filterByTag) }
-
-            // and it adds at least one role
-            // AND the new roles it would add to the script would still be less than our player count
+            .filter { c ->
+                val isNewRoleAdded = !usedSections
+                    .map { it.rolesInSection }
+                    .flatten()
+                    .toSet()
+                    .containsAll(c.rolesInSection)
+                isNewRoleAdded || canAddNothing
+            }
             .filter {
-                val newPrimaryRoleCount = (it.primaryRoles - primaryRolesInScript).size
-                val newNonPrimaryRoles = it.primaryRoles - it.rolesInSection
-
-                // this section adds something, or is allowed to add nothing
-                return@filter (newPrimaryRoleCount > 0 || canAddNothing) &&
-                        // and it wont put us over cap
-                        newPrimaryRoleCount + primaryRolesInScript.size <= playerCount &&
-                        // and all the non-primary roles already have a primary part
-                        primaryRolesInScript.containsAll(newNonPrimaryRoles)
+                val usedRoles = usedSections.map { it.rolesInSection }.flatten().toMutableSet()
+                usedRoles.addAll(it.primaryRoles)
+                usedRoles.size <= playerCount
             }
     }
 
